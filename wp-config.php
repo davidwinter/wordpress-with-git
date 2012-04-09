696af597542e0de4cafe6289f84b5e0b86593a18
David Winter <i@djw.me>
2012-04-09 14:02:35 +0100

    Update settings in wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -14,6 +14,12 @@
  * @package WordPress
  */
 
+define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/wordpress');
+define('WP_HOME',    'http://' . $_SERVER['SERVER_NAME']);
+define('WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/wp-content');
+define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp-content');
+define('WP_DEFAULT_THEME', 'set_your_theme_here');
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'database_name_here');
